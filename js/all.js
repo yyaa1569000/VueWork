db4c04104dde667b2137991ce8661e907c5dfdba
yyaa1569000 <yyaa1569@gmail.com>
2021-12-15 17:42:51 +0800

    232

--- js/all.js
+++ js/all.js
@@ -1,53 +1,54 @@
-
-var app =new Vue({
-    el:'#app',
-    data:{
-        newTodo:'',
-        newNumber:'',
-        
-        todos:[
-            // {
-            //     id:'',
-            //     title:'',
-            //     number:'',
-            //     price:''
-            // }
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        newNumber: '',
+        todos: [
+            {
+                id: '',
+                title: '測試',
+                number: '100',
+                price: '100'
+            }
         ],
-        
-        
+
+
     },
-    computed:{
-        
-    },
-    methods:{
-        addTodo:function(){
+    // computed:{
+
+    // },
+    methods: {
+        addTodo: function () {
             var value = this.newTodo.trim();
             var number = this.newNumber.trim();
             var price = parseInt(number) + parseInt(number);
             var timestamp = Math.floor(Date.now());
             console.log(value, number, timestamp);
-            if (!value){
+            if (!value) {
                 return;
-            } else if (!number){
+            } else if (!number) {
                 return;
             }
-            
+
             this.todos.push({
                 id: timestamp,
                 title: value,
                 number: number,
-                price:price,
+                price: price,
             });
-            this.newTodo=''
+            this.newTodo = ''
             this.newNumber = ''
         },
-        removeTodo: function (todo){
+        removeTodo: function (todo) {
+            var newIndex = '';
             var vm = this;
-            var newIndex = vm.todos.findIndex(function(item,key){
-                return todo.id === item.id;
+            vm.todos.forEach(function (item, key) {
+                if (todo.id === item.id) {
+                    newIndex = key
+                }
             })
-            this.todos.splice(newIndex,1);
+            this.todos.splice(newIndex, 1);
         },
-        
+
     }
 })
